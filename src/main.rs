3610cb271af0f5989fe8d13de085740e1e28c4f1
Paolo <str237@gmail.com>
2022-09-24 23:23:45 +0200

    Set /status endpoint

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ async fn greet(name: web::Path<String>) -> impl Responder {
   format!("Hello {name}!")
 }
 
+#[get("/status")]
+async fn get_status() -> impl Responder {
+  format!("Ok")
+}
+
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
+  println!("Server listening in port 8080");
   HttpServer::new(|| {
     App::new()
       .route("/hello", web::get().to(|| async { "Hello World!" }))
       .service(greet)
+      .service(get_status)
   })
   .bind(("127.0.0.1", 8080))?
   .run()
