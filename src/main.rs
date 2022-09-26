420c95b9b4cbba99342467d617671a6ba605e4ee
Paolo <str237@gmail.com>
2022-09-27 00:08:04 +0200

    Serve static files in /app/....

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 use actix::{Actor, StreamHandler};
-use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
+use actix_web::{
+  get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result as ActixResult,
+};
 use actix_web_actors::ws;
+use std::path::PathBuf;
 
 #[get("/status")]
 async fn get_status() -> impl Responder {
   format!("Ok")
+}
+
+#[get("/app/{filename:.*}")]
+async fn static_content(req: HttpRequest) -> ActixResult<NamedFile> {
+  let filename: String = req.match_info().query("filename").parse().unwrap();
+  let path = PathBuf::from(format!("./static/{filename}"));
+  println!("{}", path.to_str().unwrap());
+  Ok(NamedFile::open(path)?)
 }
 
 /// Define HTTP actor
@@ -39,6 +51,7 @@ async fn main() -> std::io::Result<()> {
     App::new()
       .route("/ws/", web::get().to(index))
       .service(get_status)
+      .service(static_content)
   })
   .bind(("127.0.0.1", 8080))?
   .run()
