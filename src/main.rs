44ac5106ca5548b5247f4d41a072d35d69792606
Paolo <str237@gmail.com>
2022-09-26 23:46:54 +0200

    Websocket example

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,43 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix::{Actor, StreamHandler};
+use actix_web::{get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
 
 #[get("/status")]
 async fn get_status() -> impl Responder {
   format!("Ok")
 }
 
-#[actix_web::main] // or #[tokio::main]
+/// Define HTTP actor
+struct MyWs;
+
+impl Actor for MyWs {
+  type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+  fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+    match msg {
+      Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+      Ok(ws::Message::Text(text)) => ctx.text(text),
+      Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+      _ => (),
+    }
+  }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+  let resp = ws::start(MyWs {}, &req, stream);
+  println!("{:?}", resp);
+  resp
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
   println!("Server listening in port 8080");
   HttpServer::new(|| {
     App::new()
-      .route("/hello", web::get().to(|| async { "Hello World!" }))
+      .route("/ws/", web::get().to(index))
       .service(get_status)
   })
   .bind(("127.0.0.1", 8080))?
