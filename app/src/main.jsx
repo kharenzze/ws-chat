a2756539533fd466ee36283a80761d8e0997192b
Paolo <str237@gmail.com>
2022-09-28 23:01:30 +0200

    Remove interval

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,5 +1,5 @@
 import { render } from 'preact'
-import { App } from './app.jsx'
+import { App } from './App.jsx'
 
 console.log("Starting ws")
 
@@ -7,14 +7,14 @@ const wsUrl = new URL('/ws/', window.location.origin.replace('http', 'ws'))
 
 const ws = new WebSocket(wsUrl)
 
-let count = 1
 
-setInterval(() => {
+ws.addEventListener('open', (evt) => {
+	let count = 1
 	const msg = "Msg no " + count
 	console.log(`Sent ${count}`)
 	ws.send(msg)
 	count++
-}, 1000)
+})
 
 ws.addEventListener('message', (evt) => {
 	console.log(evt.data)
