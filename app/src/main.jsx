2e5383b0d931008043f3555e63dab7b90c1ce273
Paolo <str237@gmail.com>
2022-09-30 18:07:52 +0200

    Remove global connection

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,22 +1,4 @@
 import { render } from 'preact'
 import { App } from './App.jsx'
 
-console.log("Starting ws")
-
-const wsUrl = new URL('/ws/', window.location.origin.replace('http', 'ws'))
-
-const ws = new WebSocket(wsUrl)
-
-ws.addEventListener('open', (_evt) => {
-	let count = 1
-	const msg = "Msg no " + count
-	console.log(`Sent ${count}`)
-	ws.send(msg)
-	count++
-})
-
-ws.addEventListener('message', (evt) => {
-	console.log(evt.data)
-})
-
 render(<App />, document.body)
