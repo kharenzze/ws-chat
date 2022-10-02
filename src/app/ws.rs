783178b55ade778676eb32c49ffdb19524237925
Paolo <str237@gmail.com>
2022-10-02 12:43:25 +0200

    Move ws actor to a mod

---
+++ src/app/ws.rs
@@ -0,0 +1,28 @@
+use crate::app::message::AppMessage;
+use actix::{Actor, StreamHandler};
+use actix_web_actors::ws;
+use chrono::prelude::*;
+
+pub struct AppWs;
+
+/// Define HTTP actor
+impl Actor for AppWs {
+  type Context = ws::WebsocketContext<Self>;
+}
+
+/// Handler for ws::Message message
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for AppWs {
+  fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+    match msg {
+      Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+      Ok(ws::Message::Text(text)) => {
+        let mut msg: AppMessage = serde_json::from_str(&text).unwrap();
+        msg.timestamp = Utc::now();
+        let json = serde_json::to_string(&msg).unwrap();
+        ctx.text(json)
+      }
+      Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+      _ => (),
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 extern crate chrono;
 mod app;
 
-use crate::app::message::AppMessage;
-use actix::{Actor, StreamHandler};
+use crate::app::ws::AppWs;
+
 use actix_files::NamedFile;
 use actix_web::{
   get, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder, Result as ActixResult,
 };
 use actix_web_actors::ws;
-use chrono::prelude::*;
 use std::path::PathBuf;
 
 const DEBUG_STATIC: bool = false;
@@ -29,32 +28,8 @@ async fn static_content(req: HttpRequest) -> ActixResult<NamedFile> {
   Ok(NamedFile::open(path)?)
 }
 
-/// Define HTTP actor
-struct MyWs;
-
-impl Actor for MyWs {
-  type Context = ws::WebsocketContext<Self>;
-}
-
-/// Handler for ws::Message message
-impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
-  fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-    match msg {
-      Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-      Ok(ws::Message::Text(text)) => {
-        let mut msg: AppMessage = serde_json::from_str(&text).unwrap();
-        msg.timestamp = Utc::now();
-        let json = serde_json::to_string(&msg).unwrap();
-        ctx.text(json)
-      }
-      Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
-      _ => (),
-    }
-  }
-}
-
 async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-  let resp = ws::start(MyWs {}, &req, stream);
+  let resp = ws::start(AppWs {}, &req, stream);
   if DEBUG_WS_TREAM {
     println!("{:?}", resp);
   }
